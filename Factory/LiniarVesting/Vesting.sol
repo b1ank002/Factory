d3e7769585545524bd8ef045c2a9c07b6dc227eb
b1ank002 <vlad.hotenko@gmail.com>
2025-06-04 21:36:00 +0200

    improve multiusers in Vesting

--- Factory/LiniarVesting/Vesting.sol
+++ Factory/LiniarVesting/Vesting.sol
@@ -1,17 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.27;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "Factory/UtilityContracts/IUtilityContract.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract Vesting is IUtilityContract, Ownable {
-
+contract Vesting is Ownable {
     constructor() Ownable(msg.sender) {}
 
     bool private initialized;
 
     IERC20 public token;
+
+    bytes32 public root;
+    
+    struct UserInfo {
+        uint256 claimed;
+        uint256 lastClaim;
+    }
+
+    mapping(address => UserInfo) public beneficiaries;
 
     uint256 public totalAmount;
     uint256 public startTime;
@@ -20,14 +28,6 @@ contract Vesting is IUtilityContract, Ownable {
     uint256 public minAmount;
     uint256 public cooldown;
 
-    struct UserInfo {
-        uint256 amount;
-        uint256 claimed;
-        uint256 lastClaim;
-    }
-
-    mapping(address => UserInfo) users;
-
     event Claim(address beneficiary, uint256 amount, uint256 timestamp);
 
     error AlreadyInitialized();
@@ -35,70 +35,45 @@ contract Vesting is IUtilityContract, Ownable {
     error NothingToClaim();
     error TransferFailed();
     error CooldownNotFinished();
+    error VerificationFailed();
 
-
-    modifier notInit() {
-        require(!initialized, AlreadyInitialized());
+    modifier checkBeneficiary(uint256 _amount, bytes32[] calldata _proofs) {
+        bytes32 leaf = keccak256(
+            abi.encodePacked(
+                keccak256(
+                    abi.encodePacked(msg.sender, _amount)
+                )
+            )
+        );
+        require(MerkleProof.verify(_proofs, root, leaf), VerificationFailed());
         _;
     }
 
-    function claim() public {
+    function claim(uint256 _amount, bytes32[] calldata _proofs) public {
         require(block.timestamp >= startTime + cliffDuration, CliffNotReached());
 
-        uint256 claimable = claimableAmount(msg.sender);
+        uint256 claimable = claimableAmount(msg.sender, _amount, _proofs);
         require(claimable >= minAmount, NothingToClaim());
-        require(users[msg.sender].lastClaim < block.timestamp - cooldown);
+        require(beneficiaries[msg.sender].lastClaim < block.timestamp - cooldown, CooldownNotFinished());
 
-        users[msg.sender].claimed += claimable;
+        beneficiaries[msg.sender].claimed += claimable;
         require(token.transfer(msg.sender, claimable), TransferFailed());
-        users[msg.sender].lastClaim = block.timestamp;
+        beneficiaries[msg.sender].lastClaim = block.timestamp;
 
         emit Claim(msg.sender, claimable, block.timestamp);
     }
 
-    function _addUsers(address[] calldata _users, uint256[] calldata _amounts) internal onlyOwner {
-        require(_users.length == _amounts.length);
-        require(!initialized);
-
-        for (uint256 i = 0; i < _users.length; i++) {
-            users[_users[i]] = UserInfo({
-                amount: _amounts[i],
-                claimed: 0,
-                lastClaim: 0
-            });
-        }
-    }  
-
-    function _vestedAmount(address _user) internal view returns(uint256) {
+    function _vestedAmount(uint256 _amount) internal view returns(uint256) {
         if (block.timestamp < startTime + cliffDuration) return 0;
 
         uint256 passedTime = block.timestamp - (startTime + cliffDuration);
 
-        return users[_user].amount * passedTime / claimDuration;
+        return _amount * passedTime / claimDuration;
     }
 
-    function claimableAmount(address _user) public view returns(uint256) {
+    function claimableAmount(address _beneficiary, uint256 _amount, bytes32[] calldata _proofs) public view checkBeneficiary(_amount, _proofs) returns(uint256) {
         if (block.timestamp < startTime + cliffDuration) return 0;
 
-        return _vestedAmount(_user) - users[_user].claimed;
-    }
-
-    function setMinClaim(uint256 _newMin) public onlyOwner {
-        minAmount = _newMin;
-    }
-
-    function setCooldown(uint256 _newCooldown) public onlyOwner {
-        cooldown = _newCooldown;
-    }
-
-    function initialize(bytes memory _initData) external notInit returns(bool) {
-        (address _token, uint256 _amount, address _treasury, address _owner) = abi.decode(_initData, (address, uint256, address, address));
-
-        return initialized = true;
-    }
-
-    function getInitData(address _token, uint256 _amount, address _treasury, address _owner) external pure returns(bytes memory) {
-        return abi.encode(_token, _amount, _treasury, _owner);
+        return _vestedAmount(_amount) - beneficiaries[_beneficiary].claimed;
     }
 }
-
